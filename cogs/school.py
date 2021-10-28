bc4b681d13ad52a7edb0203ade7d33bc4138bb22
gutsy47 <bestcom5055@gmail.com>
2021-10-28 20:54:15 +0300

    Improve: school.py algorithms
    DO NOT USE THIS ONE
    
    Added def date_format, which turns datetime object to string formatted to AA DD.MM.YY (rus)
    Improved get_homework structure:
     -Changed some names of return:
      lesson => name
      attachments => files
     -Working with datetime object from now
     -Improved work time (break if message found)
     -Fixed bug due to which the content was written year in the absence of text after the title
    Improved homework_and_schedule:
     -The check frequency is now 15 minutes
     -Do not avoid the first loop
     -Working with datetime object
     -When checking for the presence of the posted schedule, only the last message is checked
     -Unloading to the DB will occur inside functions
     -Improved working time (Do not working with homework)
    
    The rest of the changes are sketches as I continue to work.
    
    Signed-off-by: gutsy47 <bestcom5055@gmail.com>

--- cogs/school.py
+++ cogs/school.py
@@ -8,19 +8,6 @@ import requests
 import bs4
 from bs4 import BeautifulSoup
 
-lessons = {
-        '📙physics': 'Физика', '📙maths': 'Математика', '📙ict': 'Информатика',
-        '📗russian': 'Русский', '📗english': 'Английский',
-        '📕literature': 'Литература', '📕history': 'История',
-        '📘biology': 'Биология', '📘chemistry': 'Химия',
-        '📒astronomy': 'Астрономия', '📒geography': 'География',
-    }
-days = {
-    'Mon': 'понедельник', 'Tue': 'вторник', 'Wed': 'среда', 'Thu': 'четверг', 'Fri': 'пятница',
-    'Sat': 'суббота', 'Sun': 'воскресенье'
-}
-courses = ('5а', '5б', '6а', '6б', '7а', '7б', '8а', '8б', '9а', '9б', '10м', '10х', '10э', '11м', '11х', '11э')
-
 
 class School(commands.Cog):
     """
@@ -30,63 +17,76 @@ class School(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    async def get_homework(self, date):
+    @staticmethod
+    async def date_format(date: datetime):
+        """Returns str(date) formatted to AA DD.MM.YY"""
+        week = {
+            'Mon': 'понедельник', 'Tue': 'вторник', 'Wed': 'среда', 'Thu': 'четверг', 'Fri': 'пятница',
+            'Sat': 'суббота', 'Sun': 'воскресенье'
+        }
+        return week[datetime.strftime(date, '%a')] + datetime.strftime(date, ' %d.%m.%y')
+
+    async def get_homework(self, date: datetime):
         """Returns homework for a given date or ValueError if format is wrong
         Format of homework: [{
-            lesson: str lesson name,
-            content: str homework text, (can be empty)
-            attachments: list homework files(photos), (can be empty)
-            source: str link to the original message
-        },]
+            name: str - lesson name,
+            content: str - homework text (can be empty),
+            files: list - homework files (can be empty list),
+            source: str - link to the original message
+        }, {...}]
 
-        :param date: str - Date in DD.MM.YY format
+        :param date: datetime - DateTime object
         :return: dict(homework) or commands.BadArgument(Format error), str(date)
         """
-        try:  # Argument error handler
-            datetime.strptime(date, '%d.%m.%y')
-        except ValueError:
-            return commands.BadArgument("**Date** should have **dd.mm.yy** format"), date
-
+        lessons = {
+            '📙physics': 'Физика', '📙maths': 'Математика', '📙ict': 'Информатика',
+            '📗russian': 'Русский', '📗english': 'Английский',
+            '📕literature': 'Литература', '📕history': 'История',
+            '📘biology': 'Биология', '📘chemistry': 'Химия',
+            '📒astronomy': 'Астрономия', '📒geography': 'География',
+        }
         homework = []
-        for channel in self.bot.get_channel(self.bot.HomeworkID).text_channels:  # Channels of SCHOOL category
-            async for message in channel.history(limit=10):
-                if date in message.content:
-                    hw = {'lesson': str(message.channel), 'content': '', 'attachments': [], 'source': message.jump_url}
-                    if message.content[message.content.find('\n')] != '1':
-                        hw['content'] = message.content[message.content.find('\n'):]
-                    if message.attachments:
-                        hw['attachments'] = [attachment.url for attachment in message.attachments]
-                    homework.append(hw)
+        for channel in self.bot.get_channel(self.bot.HomeworkID).text_channels:
+            async for message in channel.history():
+                if "дз" in message.content and datetime.strftime(date, '%d.%m.%y') in message.content:
+                    name = lessons[str(message.channel)]
+                    content = ''
+                    if '\n' in message.content:
+                        content = message.content[message.content.find('\n'):]  # Cut off the title
+                    files = [file.url for file in message.attachments]
+                    src = message.jump_url
+                    homework.append({'name': name, 'content': content, 'files': files, 'source': src})
+                    break
 
         return homework
 
-    async def get_schedule(self, date: str = None, course: str = None):
+    async def get_schedule(self, date: datetime, course: str):
         """Returns the timetable from the school website
 
         :param date: str - The date for which you need to get the schedule (format: DD.MM.YY)
         :param course: str - School course to be returned
-        :return: tables and date
+        :return: Table
         """
+        # Argument error handler
+        check = ('5а', '5б', '6а', '6б', '7аs', '7б', '8а', '8б', '9а', '9б', '10м', '10х', '10э', '11м', '11х', '11э')
+        if date.weekday() > 4:
+            return commands.BadArgument("No lessons on weekends")
+        if course not in check:
+            return commands.BadArgument("Incorrect course")
+
         response = requests.get(self.bot.ScheduleURL)
         soup = BeautifulSoup(response.text, 'lxml')
 
         tables = soup.find_all('tbody')
-        headers = [h2 for h2 in soup.find_all('h2') if "Расписание" in h2.get_text()]
-        content = {}  # {day: [table, table]}
-        for index, header in enumerate(headers):
-            day = header.get_text().split()[-2]
-            content[day] = [tables[index*2], tables[index*2 + 1]]
-
-        try:  # Argument error handler
-            date = datetime.strptime(date, '%d.%m.%y') if date else datetime.today() + timedelta(days=1)
-        except ValueError:
-            return commands.BadArgument("**Date** should have **dd.mm.yy** format")
-        if date.weekday() > 4:
-            return commands.BadArgument("No lessons on weekends")
-        if str(date.day) not in content.keys():
+        dates = [h2.get_text().split()[-2] for h2 in soup.find_all('h2') if "Расписание" in h2.get_text()]
+        if str(date.day) not in dates:
             return commands.BadArgument("Schedule not posted for the selected date")
-        if course and course not in courses:
-             return commands.BadArgument("Incorrect course")
+
+        # TODO: Соединить две таблицы и обработать (пропуская создание content)
+        content = {}  # {day: [table, table]}
+        for index, header in enumerate(dates):
+            day = header
+            content[day] = [tables[index*2], tables[index*2 + 1]]
 
         tables = []
         for table in content[str(date.day)]:
@@ -108,62 +108,43 @@ class School(commands.Cog):
         for table in tables:  # Reformat to {course: [lessons]}
             for row in table[1:]:
                 schedule[row[0]] = [lesson.capitalize() for lesson in row[2:]]
-
-        return {course: schedule[course]} if course else schedule
+        return schedule[course] if course else schedule
 
     @commands.Cog.listener()
     async def on_ready(self):
         self.homework_and_schedule.start()
 
-    @tasks.loop(hours=1)
+    @tasks.loop(minutes=15)
     async def homework_and_schedule(self):
-        """Automatically sends schedule for 11m class and homework as paginated embed to #schedule channel"""
-        if self.homework_and_schedule.current_loop == 0:  # Avoid the first loop
+        """Schedule distribution
+        Sends timetable for the 11m course to the #Schedule channel.
+        Also adds buttons(reactions) to view homework or hide data
+        """
+        channel = self.bot.get_channel(self.bot.ScheduleID)  # Schedule channel
+
+        # Get tomorrow date (or monday if tomorrow is weekend)
+        date = datetime.today() + timedelta(days=1)
+        if date.weekday() > 5:
+            date += timedelta(days=7 - date.weekday())
+
+        # Avoid existing message
+        last_message = await channel.fetch_message(channel.last_message_id)
+        if datetime.strftime(date, '%d.%m.%y') in last_message.embeds[0].title:
             return
 
-        tomorrow = datetime.today() + timedelta(days=1)
-        if tomorrow.weekday() < 5:
-            tomorrow = datetime.strftime(tomorrow, '%d.%m.%y')
-        else:  # Monday if tomorrow is weekend
-            tomorrow = datetime.strftime(tomorrow + timedelta(days=7 - tomorrow.weekday()), '%d.%m.%y')
-
-        async for message in self.bot.get_channel(self.bot.ScheduleID).history(limit=None):  # Avoid existing messages
-            if tomorrow in message.embeds[0].title:
-                return
-
-        homework = await self.get_homework(date=tomorrow)
-        timetables = await self.get_schedule(date=tomorrow, course='11м')
-        if not isinstance(timetables, dict):
+        # Get data
+        timetable = await self.get_schedule(date=date, course='11м')
+        if not isinstance(timetable, list):
             return
 
-        date = days[datetime.strftime(datetime.strptime(tomorrow, '%d.%m.%y'), '%a')] + ' ' + tomorrow
-        to_dump = {'homework': [], 'schedule': {'description': ''}}
+        # Message create
+        date = await self.date_format(date=date)
+        embed = discord.Embed(title=f"Расписание {date}", description='', color=self.bot.ColorDefault)
+        for index, lesson in enumerate(timetable):
+            embed.description += f"\n`{index + 1}` {lesson}" if lesson else ''
 
-        description = ''
-        for key, value in timetables.items():
-            for index, lesson in enumerate(value):
-                description += f"\n`{index + 1}` {lesson}" if lesson else ''
-        to_dump['schedule']['description'] = description
-        embed = discord.Embed(title=f"Расписание {date}", description=description, color=self.bot.ColorDefault)
-
-        if homework:
-            for lesson in homework:
-                if lessons[lesson['lesson']][:3] in description:
-                    value = lesson['content'] + f" ([Источник]({lesson['source']}))"
-                    if lesson['attachments']:
-                        value += "\nПрикреплённые файлы: "
-                        for index, link in enumerate(lesson['attachments']):
-                            value += f"[№{index + 1}]({link})"
-                            value += ', ' if index + 1 < len(lesson['attachments']) else ''
-                    to_dump['homework'].append({'name': lessons[lesson['lesson']], 'value': value})
-        else:
-            to_dump['homework'].append({'name': "Ничего не задано", 'value': r"¯\_(ツ)_/¯"})
-
-        file_name = '.'.join(date.split()[1].split('.')[::-1]) + ' ' + date.split()[0]
-        with open(f"schedules/{file_name}.json", 'w', encoding='UTF-8') as file:
-            json.dump(to_dump, file, ensure_ascii=False, indent=2)
-
-        message = await self.bot.get_channel(self.bot.ScheduleID).send(embed=embed)
+        # Send message and add buttons
+        message = await channel.send(embed=embed)
         for button in ('🔼', '➡️'):
             await message.add_reaction(emoji=button)
 
@@ -179,16 +160,16 @@ class School(commands.Cog):
         try:
             title = message.embeds[0].title.split()
             current = title[0] + ' '
-            file_name = '.'.join(title[2].split('.')[::-1]) + ' ' + title[1]
-            date = ' '.join(title[1:])
             if current not in ("Расписание ", "ДЗ "):
                 return
+            file_name = '.'.join(title[2].split('.')[::-1]) + ' ' + title[1]
+            date = ' '.join(title[1:])
         except IndexError:
             return
         except AttributeError:
             return
 
-        if (member == self.bot.user) or (not message.embeds):
+        if member == self.bot.user:
             return
         if (not reaction.me or reaction.count > 1) and message.author.id == self.bot.user.id:
             if reaction.emoji != '🔄':
@@ -273,13 +254,18 @@ class School(commands.Cog):
         ],
         description="Sends schedule"
     )
+    @commands.cooldown(1, 30, commands.BucketType.user)
     async def schedule(self, ctx, course: str, date: str = None):
-        """Sends schedule for 11m class and homework as paginated embed to #schedule channel
+        """Sends schedule for class
 
         :param ctx: discord.ext.commands.Context - Represents the context in which a command is being invoked under
         :param course: str - Course name in format as on the school website
         :param date: str - Date in DD.MM.YY format (tomorrow as default)
         """
+        # try:
+        #     date = datetime.strptime(date, '%d.%m.%y')
+        # except ValueError:
+        #     return commands.BadArgument("**Date** should have **dd.mm.yy** format")
         date = datetime.strptime(date, '%d.%m.%y') if date else datetime.today()
         if date.weekday() < 5:
             date = datetime.strftime(date, '%d.%m.%y')
