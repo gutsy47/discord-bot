13157122f46666ac96f425d0eb587ca02fe72835
gutsy47 <bestcom5055@gmail.com>
2021-10-31 22:37:06 +0300

    Initial commit
    Signed-off-by: gutsy47 <bestcom5055@gmail.com>

---
+++ cogs/english.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+import discord
+from discord.ext import commands
+import gspread
+import os
+
+
+class English(commands.Cog):
+    """Helps to learn a set of new english words"""
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+        credentials = {
+            "type": "service_account",
+            "project_id": os.environ['GOOGLE_PROJECT_ID'],
+            "private_key_id": os.environ["GOOGLE_PRIVATE_KEY_ID"],
+            "private_key": os.environ["GOOGLE_PRIVATE_KEY"].replace('\\n', '\n'),
+            "client_email": os.environ["GOOGLE_CLIENT_EMAIL"],
+            "client_id": os.environ["GOOGLE_CLIENT_ID"],
+            "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+            "token_uri": "https://oauth2.googleapis.com/token",
+            "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+            "client_x509_cert_url": os.environ['GOOGLE_CLIENT_X509_CERT_URL']
+        }
+        service_account = gspread.service_account_from_dict(credentials)
+        self.spreadsheet = service_account.open("English")
+
+
+def setup(bot):
+    bot.add_cog(English(bot))
